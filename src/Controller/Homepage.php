d0766cbd1eccbd61ab95cdb8a3597854b798e5ce
Giacomo Moscardini <gmoscardini@webgriffe.com>
2021-01-18 19:44:59 +0100

    Add homepage route

---
+++ src/Controller/Homepage.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+final class Homepage extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig');
+    }
+}
