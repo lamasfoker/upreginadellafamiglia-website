0cd7aee06122ca643e34c01ceed95f6066a34ca4
Giacomo Moscardini <gmoscardini@webgriffe.com>
2021-02-02 17:52:56 +0100

    Add news route

---
+++ src/Controller/News.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+final class News extends AbstractController
+{
+    public function list(): Response
+    {
+        return $this->render('news/list.html.twig');
+    }
+}
