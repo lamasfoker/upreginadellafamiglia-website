21cfb1f69f63da8a948fbd5f1eb7fb52cb7c6cd5
Giacomo Moscardini <gmoscardini@webgriffe.com>
2021-03-25 21:45:41 +0100

    Change colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,30 +32,13 @@ module.exports = {
                 '1/7': '14.2857143%'
             },
             colors: {
-                "theme-1": {
-                    light: '#f2aa98',
-                    DEFAULT: '#E75532',
-                    dark: '#7d240e'
+                "color-1": {
+                    light: '#eaeaf2',
+                    DEFAULT: '#5a94c7',
+                    dark: '#165185'
                 },
-                "theme-2": {
-                    light: '#ede6c4',
-                    DEFAULT: '#DBCD8A',
-                    dark: '#88782a'
-                },
-                "theme-3": {
-                    light: '#a4dcd8',
-                    DEFAULT: '#4ABAB3',
-                    dark: '#235e5a'
-                },
-                "theme-4": {
-                    light: '#80a4d4',
-                    DEFAULT: '#2B4F7F',
-                    dark: '#15273f'
-                },
-                "theme-5": {
-                    light: '#a98686',
-                    DEFAULT: '#3A2929',
-                    dark: '#1d1414'
+                "color-2": {
+                    DEFAULT: '#e85a37'
                 }
             },
             fontFamily: {
