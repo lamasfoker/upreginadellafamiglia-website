26787c0e6be98a7018c6628918828f8bcad146d4
Giacomo Moscardini <gmoscardini@webgriffe.com>
2021-04-07 08:46:42 +0200

    Dynamize footer

---
+++ src/Controller/Footer.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Repository\CmsPageRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\DecoderInterface;
+use Symfony\Component\Serializer\Encoder\JsonDecode;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+
+final class Footer extends AbstractController
+{
+    private DecoderInterface $decoder;
+
+    public function __construct(DecoderInterface $decoder)
+    {
+        $this->decoder = $decoder;
+    }
+
+    public function index(): Response
+    {
+        $json = $this->decoder->decode(
+            file_get_contents(CmsPageRepositoryInterface::CMS_PAGE_FILE_LOCATION),
+            JsonEncoder::FORMAT,
+            [JsonDecode::ASSOCIATIVE => true]
+        );
+
+        if (!is_array($json)) {
+            return $this->json('NOT FOUND');
+        }
+
+        return $this->render('footer.html.twig', ['footer' => $json['footer']]);
+    }
+}
