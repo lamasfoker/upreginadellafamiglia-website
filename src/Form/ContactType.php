11909c6863a70ee7271d1ca864d7ecc1d9f4575b
Giacomo Moscardini <gmoscardini@webgriffe.com>
2021-04-11 08:11:13 +0200

    Create form builder for contact page

---
+++ src/Form/ContactType.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('mail', EmailType::class)
+            ->add('page', HiddenType::class)
+            ->add('message', TextareaType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+}
