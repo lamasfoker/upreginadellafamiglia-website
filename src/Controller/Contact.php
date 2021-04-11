2a4e47f8c9a593cd850d7e615ab3718bf512aed1
Giacomo Moscardini <gmoscardini@webgriffe.com>
2021-04-11 08:13:41 +0200

    Dinamize form and send mail after submit it

--- src/Controller/Contact.php
+++ src/Controller/Contact.php
@@ -4,24 +4,86 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use App\Repository\CmsPageRepositoryInterface;
 use App\Service\BreadcrumbsGetter;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class Contact extends AbstractController
 {
     private BreadcrumbsGetter $breadcrumbsGetter;
 
-    public function __construct(BreadcrumbsGetter $breadcrumbsGetter)
-    {
+    private CmsPageRepositoryInterface $cmsPageRepository;
+
+    private MailerInterface $mailer;
+
+    private TranslatorInterface $translator;
+
+    private string $noReplayMail;
+
+    private string $defaultRecipient;
+
+    public function __construct(
+        BreadcrumbsGetter $breadcrumbsGetter,
+        CmsPageRepositoryInterface $cmsPageRepository,
+        MailerInterface $mailer,
+        TranslatorInterface $translator,
+        string $noReplayMail,
+        string $defaultRecipient
+    ) {
         $this->breadcrumbsGetter = $breadcrumbsGetter;
+        $this->cmsPageRepository = $cmsPageRepository;
+        $this->mailer = $mailer;
+        $this->translator = $translator;
+        $this->noReplayMail = $noReplayMail;
+        $this->defaultRecipient = $defaultRecipient;
     }
 
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(ContactType::class);
+
+        $page = $request->get('page');
+        if ($page) {
+            $form->setData(['page' => $page]);
+        }
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $mail = $this->defaultRecipient;
+            $data = $form->getData();
+            if (is_array($data) && is_string($data['page'])) {
+                $page = $this->cmsPageRepository->getBySlug($data['page']);
+                if ($page) {
+                    $mail = $page[CmsPageRepositoryInterface::CONTENTFUL_RESOURCE_REFERENT_MAIL_FIELD_ID];
+                }
+            }
+
+            $mail = (new TemplatedEmail())
+                ->from($this->noReplayMail)
+                ->to(new Address($mail))
+                ->subject($this->translator->trans('app.email.subject'))
+                ->htmlTemplate('emails/contact.html.twig')
+                ->context(['data' => $data]);
+
+            $this->mailer->send($mail);
+            $this->addFlash(
+                'notice',
+                $this->translator->trans('app.contact_page.flash')
+            );
+
+            return $this->redirectToRoute('homepage');
+        }
+
         return $this->render(
             'contact/index.html.twig',
-            ['breadcrumbs' => $this->breadcrumbsGetter->getContactPageBreadcrumbs()]
+            ['breadcrumbs' => $this->breadcrumbsGetter->getContactPageBreadcrumbs(), 'form' => $form->createView()]
         );
     }
 }
--- src/Repository/CmsPageRepositoryInterface.php
+++ src/Repository/CmsPageRepositoryInterface.php
@@ -14,6 +14,8 @@ interface CmsPageRepositoryInterface extends RepositoryInterface
 
     public const CONTENTFUL_RESOURCE_TITLE_FIELD_ID = 'titolo';
 
+    public const CONTENTFUL_RESOURCE_REFERENT_MAIL_FIELD_ID = 'email';
+
     public const CMS_PAGE_FILE_LOCATION = __DIR__ . '/../../var/data/links.json';
 
     public function getBySlug(string $slug): ?ResourceInterface;
